0f5396e27ccfe5ff084b7b8b0309e06bc8abb2e1
Trey Shugart <treshugart@gmail.com>
2013-07-22 12:04:46 +1000

    Update test runner.

--- bin/tests.php
+++ bin/tests.php
@@ -6,25 +6,58 @@ use Testes\Autoloader;
 
 $base = __DIR__ . '/..';
 
-require $base . '/vendor/autoload.php';
+require $base . '/vendor/treshugart/testes/src/Testes/Autoloader.php';
 
 Autoloader::register();
 Autoloader::addPath($base . '/tests');
+Autoloader::addPath($base . '/src');
 
-$coverage = (new Coverage)->start();
-$suite    = (new Finder($base . '/tests'))->run();
+$coverage = new Coverage;
+$finder   = new Finder($base . '/tests', 'Test');
+
+$coverage->start();
+
+echo PHP_EOL;
+
+$suite = $finder->run(function($test) {
+    echo $test->getAssertions()->isPassed() && !$test->getExceptions()->count() ? '.' : 'F';
+});
+
+echo PHP_EOL . PHP_EOL . sprintf('Ran %d test%s.', count($suite), count($suite) === 1 ? '' : 's');
+
 $analyzer = $coverage->stop()->addDirectory($base . '/src')->is('\.php$');
 
-?>
+echo PHP_EOL . PHP_EOL . 'Coverage: ' . $analyzer->getPercentTested() . '%' . PHP_EOL . PHP_EOL;
 
-<?php if ($suite->getAssertions()->isPassed()): ?>
-All tests passed!
-<?php else: ?>
-Tests failed:
-<?php foreach ($suite->getAssertions()->getFailed() as $ass): ?>
-  <?php echo $ass->getTestClass(); ?>
-<?php endforeach; ?>
-<?php endif; ?>
+if (count($assertions = $suite->getAssertions()->getFailed())) {
+    echo 'Assertions' . PHP_EOL;
+    echo '----------' . PHP_EOL;
 
-Coverage: <?php echo $analyzer->getPercentTested(); ?>%
+    foreach ($assertions as $ass) {
+        echo '  ' . $ass->getTestClass() . ':' . $ass->getTestLine() . ' ' . $ass->getMessage() . PHP_EOL;
+    }
 
+    echo PHP_EOL;
+}
+
+if (count($exceptions = $suite->getExceptions())) {
+    echo 'Exceptions' . PHP_EOL;
+    echo '----------' . PHP_EOL;
+
+    foreach ($exceptions as $exc) {
+        echo '  ' . $exc->getTestFile() . ':' . $exc->getTestLine() . ' ' . $exc->getMessage() . PHP_EOL;
+    }
+
+    echo PHP_EOL;
+}
+
+if (count($benchmarks = $suite->getBenchmarks())) {
+    echo 'Benchmarks' . PHP_EOL;
+    echo '----------' . PHP_EOL;
+
+    foreach ($benchmarks as $name => $bench) {
+        echo '  ' . $name . ': ' . round($bench->getTime(), 3) . PHP_EOL;
+    }
+
+    echo PHP_EOL;
+}
